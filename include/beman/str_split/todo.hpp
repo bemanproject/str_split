b25dc17d4ff7a48a1008b81c67fd67fed488ef18
Aryan Naraghi <aryan.naraghi@gmail.com>
2026-05-09 18:40:44 -0600

    Adds more details to the concepts that govern the range constructor

--- include/beman/str_split/todo.hpp
+++ include/beman/str_split/todo.hpp
@@ -16,6 +16,26 @@ import beman.str_split;
 
 namespace beman::str_split {
 
+// TODO(aryann): Delete this file and move the contents to str_split.hpp once the we make more progress on the surface
+// definition.
+
+//------------------------------------------------------------------------------
+// Concepts:
+//------------------------------------------------------------------------------
+
+// TODO(aryann): Should the concepts be placed in a private namespace?
+
+// A range of chars.
+template <typename T>
+concept char_range = std::ranges::input_range<T> && std::same_as<std::ranges::range_value_t<T>, char>;
+
+// A type that cannot be converted to `std::string_view`.
+template <typename T>
+concept not_string_view_convertible = !std::convertible_to<T&&, std::string_view>;
+
+template <typename T, typename Self>
+concept different_from = !std::same_as<std::remove_cvref_t<T>, Self>;
+
 //------------------------------------------------------------------------------
 // Patterns:
 //------------------------------------------------------------------------------
@@ -30,23 +50,31 @@ struct split_by {
 
     // Constructor for range of characters that are not `std::string_view` convertible.
     template <std::ranges::input_range Range>
-        requires std::same_as<std::ranges::range_value_t<Range>, char> &&
-                 (!std::convertible_to<Range &&, std::string_view>) &&
-                 (!std::same_as<std::remove_cvref_t<Range>, split_by>)
-    explicit split_by(Range&& range) : delimiter_(std::ranges::begin(range), std::ranges::end(range)) {}
+        requires(
+            // Ensures the range's value type is `char`. Notably, this rejects ranges of other values types such as
+            // `int` and `unsigned char`. This requirement prevents narrowing conversions.
+            char_range<Range> &&
+
+            // Ensures this constructor does not compete with the `std::string_view` overload.
+            not_string_view_convertible<Range> &&
+
+            // Ensures this constructor does not hijack copy and move construction which would fail to compile with a
+            // difficult-to-read wall of errors.
+            different_from<Range, split_by>)
+    constexpr explicit split_by(Range&& range) : delimiter_(std::ranges::begin(range), std::ranges::end(range)) {}
 
     // TODO(aryann): Here and below, implement a find member function that accepts the current "haystack" string and
     // returns the position of the first match. We may also need to control the visibility of such function.
 
   private:
-    std::string delimiter_;
+    const std::string delimiter_;
 };
 
 // Splits by the first matching character in a given character sequence.
 struct split_by_first_of {
 
   private:
-    std::string chars_;
+    const std::string chars_;
 };
 
 struct split_by_ascii_whitespace {};
@@ -83,6 +111,10 @@ constexpr Container str_split(std::string_view input) {
 }
 
 // TODO(aryann): Add support for max splits.
+
+// TODO(aryann): For now, the caller must pass the return type. We should decide whether to use implicit conversions to
+// adapt to the user's declared return type (similar to absl's approach). If we retain the current structure, we should
+// decide on a default for the common case (`std::vector<std::string_view>`?).
 
 } // namespace beman::str_split
 
