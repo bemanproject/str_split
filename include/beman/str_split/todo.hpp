e6632f4c75acbf6339bffd8d99db2ae9d481a519
Aryan Naraghi <aryan.naraghi@gmail.com>
2026-05-09 18:20:31 -0600

    Adds an MVP set of functions
    The purpose of this change is to start the conversation on the best interface for this library.

--- include/beman/str_split/todo.hpp
+++ include/beman/str_split/todo.hpp
@@ -5,6 +5,9 @@
 
 #include <beman/str_split/config.hpp>
 
+#include <string>
+#include <string_view>
+
 #if BEMAN_STR_SPLIT_USE_MODULES() && !defined(BEMAN_STR_SPLIT_INCLUDED_FROM_INTERFACE_UNIT)
 
 import beman.str_split;
@@ -13,7 +16,73 @@ import beman.str_split;
 
 namespace beman::str_split {
 
-// TODO
+//------------------------------------------------------------------------------
+// Patterns:
+//------------------------------------------------------------------------------
+
+// Splits by a substring.
+struct split_by {
+    // Constructor for anything that can be converted to a `std::string_view`.
+    constexpr explicit split_by(std::string_view delimiter) : delimiter_(delimiter) {}
+
+    // Constructor for the single character case.
+    constexpr explicit split_by(char delimiter) : delimiter_(1, delimiter) {}
+
+    // Constructor for range of characters that are not `std::string_view` convertible.
+    template <std::ranges::input_range Range>
+        requires std::same_as<std::ranges::range_value_t<Range>, char> &&
+                 (!std::convertible_to<Range &&, std::string_view>) &&
+                 (!std::same_as<std::remove_cvref_t<Range>, split_by>)
+    explicit split_by(Range&& range) : delimiter_(std::ranges::begin(range), std::ranges::end(range)) {}
+
+    // TODO(aryann): Here and below, implement a find member function that accepts the current "haystack" string and
+    // returns the position of the first match. We may also need to control the visibility of such function.
+
+  private:
+    std::string delimiter_;
+};
+
+// Splits by the first matching character in a given character sequence.
+struct split_by_first_of {
+
+  private:
+    std::string chars_;
+};
+
+struct split_by_ascii_whitespace {};
+
+//------------------------------------------------------------------------------
+// Split functions:
+//------------------------------------------------------------------------------
+
+// TODO(aryann): Consider an alternative approach where the split type is determined by the function name:
+//
+//   * split(std::string_view): Equivalent to
+//     str_split(std::string_view, split_by_ascii_whitespace).
+//
+//   * split(std::string_view, Range&&): Equivalent to
+//     str_split(std::string_view, split_by).
+//
+//   * split_by_first_of(std::string_view, Range&&): Equivalent to
+//     str_split(std::string_view, split_by_first_of).
+//
+// I'm partial towards this alternative as it more closely mirrors `std::string::find` and `:find_first_of`.
+
+// TODO(aryann): We should structure the input similar to `split_by` by allowing both ranges and
+// `std::string_view`-convertible types.
+
+template <typename Container, typename Pattern>
+constexpr Container str_split(std::string_view input, Pattern pattern) {
+    Container empty;
+    return empty;
+}
+
+template <typename Container>
+constexpr Container str_split(std::string_view input) {
+    return str_split<Container>(input, split_by_ascii_whitespace{});
+}
+
+// TODO(aryann): Add support for max splits.
 
 } // namespace beman::str_split
 
--- tests/beman/str_split/todo.test.cpp
+++ tests/beman/str_split/todo.test.cpp
@@ -1,10 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <string_view>
+#include <vector>
+
 #include <beman/str_split/config.hpp>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 #include <beman/str_split/todo.hpp>
 
-TEST(TodoTest, todo) {
-    const bool todo = true;
-    EXPECT_TRUE(todo);
+namespace {
+
+using ::beman::str_split::split_by;
+using ::beman::str_split::str_split;
+using ::testing::ElementsAre;
+
+TEST(StrSplit, SplitBy) {
+    using Container = std::vector<std::string_view>;
+
+    // `std:string-view`-convertible inputs:
+    EXPECT_THAT(str_split<Container>("my string", split_by(" ")), ElementsAre());
+    EXPECT_THAT(str_split<Container>("my string", split_by("string")), ElementsAre());
+    EXPECT_THAT(str_split<Container>("my string", split_by(std::string("string"))), ElementsAre());
+    EXPECT_THAT(str_split<Container>("my string", split_by(std::string_view("string"))), ElementsAre());
+
+    // Single char:
+    EXPECT_THAT(str_split<Container>("my string", split_by('s')), ElementsAre());
+
+    // Ranges:
+    EXPECT_THAT(str_split<Container>("my string", split_by(std::vector<char>{'a', 'b', 'c'})), ElementsAre());
+    EXPECT_THAT(str_split<Container>("my string", split_by(std::array{'a', 'b', 'c'})), ElementsAre());
 }
+
+} // namespace
