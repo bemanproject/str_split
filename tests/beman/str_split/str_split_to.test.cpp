bb60de365eae38d6d6b8b01af230376faa538bbb
Aryan Naraghi <aryan.naraghi@gmail.com>
2026-05-11 10:11:48 -0700

    Adds support for importing std types via modules

--- tests/beman/str_split/str_split_to.test.cpp
+++ tests/beman/str_split/str_split_to.test.cpp
@@ -1,10 +1,20 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include <array>
-#include <deque>
-#include <string_view>
-#include <type_traits>
-#include <vector>
+#include <beman/str_split/config.hpp>
+
+#if BEMAN_STR_SPLIT_USE_MODULES()
+
+import std;
+
+#else
+
+    #include <array>
+    #include <deque>
+    #include <string_view>
+    #include <type_traits>
+    #include <vector>
+
+#endif // BEMAN_STR_SPLIT_USE_MODULES()
 
 #include <beman/str_split/config.hpp>
 #include <beman/str_split/str_split_to.hpp>
